c3dcf10905fec64c74cf5f1b45c5216949bac619
Rajae <Rajae@CRI16-08>
2017-10-17 15:14:02 +0200

    Adding Factory

---
+++ BananaSnake/Controller/FruitFactory.cs
@@ -0,0 +1,48 @@
+﻿using BananaSnake.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaSnake.Controller
+{
+    class FruitFactory
+    {
+        public Fruit CreateFruit(GameArea area)
+        {
+            //Random position 
+            Random randX = new Random();
+            Random randY = new Random();
+
+
+            int X = randX.Next(1, area.Width);
+            int Y = randX.Next(1, area.Height);
+
+
+            // Random Color 
+            Random randColor = new Random();
+            int color = randColor.Next(1, 4);
+            ConsoleColor randomColor; 
+
+            switch (color)
+            {
+                case 1 :
+                    randomColor = ConsoleColor.Yellow;
+                    break;
+                case 2:
+                    randomColor = ConsoleColor.Red;
+                    break;
+                case 3:
+                    randomColor = ConsoleColor.Green;
+                    break;
+                default:
+                    throw new Exception("invalid default color value ");
+                    break; 
+            }
+
+            return new Fruit(X,Y,randomColor);
+
+        }
+    }
+}
--- BananaSnake/Model/Fruit.cs
+++ BananaSnake/Model/Fruit.cs
@@ -8,14 +8,14 @@ namespace BananaSnake.Model
 {
     class Fruit
     {
-
+        public ConsoleColor FruitColor;
         public Position FruitPosition { get; set; }
 
 
-        public Fruit(int xposition, int yposition)
+        public Fruit(int xposition, int yposition , ConsoleColor color)
         {
             this.FruitPosition = new Position(xposition, yposition);
-
+            this.FruitColor = color; 
 
         }
 
