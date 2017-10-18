a88058441ec7a227c1d0562213272da7f68bb2c7
Manuel ORY <manuory@live.fr>
2017-10-18 11:27:41 +0100

    Ajout de commentaires part 1

--- BananaSnake/Controller/FruitControler.cs
+++ BananaSnake/Controller/FruitControler.cs
@@ -11,13 +11,21 @@ namespace BananaSnake.Controller
 {
     class FruitControler
     {
+        /// <summary>
+        /// Met à jour le fruit, en gardant l'ancien fruit ou en créant un autre
+        /// </summary>
+        /// <param name="fruit">fruit à Mettre à jour</param>
+        /// <param name="isFruitEat">Indique si le fruit a été mangé</param>
+        /// <param name="fruitFactory">Objet servant à rectéer un nouveau fruit si nécessaire</param>
+        /// <param name="gameArea">La zone de jeu</param>
+        /// <param name="score">le score à mettre à jour</param>
+        /// <returns>Le fruit mis à jour</returns>
         public Fruit UpdateFruit(Fruit fruit, bool isFruitEat, FruitFactory fruitFactory,  GameArea gameArea, Score score)
         {
             if (Game.isFruitEat)
             {
                 fruit = fruitFactory.CreateFruit(gameArea);
                 score.ScoreValue+=fruit.earnedPoints;
-
             }
             else
             {
